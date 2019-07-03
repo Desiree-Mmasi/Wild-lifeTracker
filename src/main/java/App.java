7e7563cdc495db13862b5c7e8dcf1cb9b7216043
Moringa School <mmasidesiree9@gmail.com>
2019-07-03 12:56:28 +0300

    This commit will add the routing for the home page

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -6,11 +6,18 @@ import spark.template.handlebars.HandlebarsTemplateEngine;
 import static spark.Spark.*;
 
 public class App {
-public static void main(String[] args) {
-    staticFileLocation("/public");
-   get("/", (request, response) -> {
-       return new ModelAndView(new HashMap(), "index.hbs");
-   }, new HandlebarsTemplateEngine());
+    public static void main(String[] args) {
+        staticFileLocation("/public");
 
-}
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(new HashMap(), "index.hbs");
+        }, new HandlebarsTemplateEngine());
+
+
+        get("/home",(request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(new HashMap(), "index.hbs");
+        }, new HandlebarsTemplateEngine());
+    }
 }
