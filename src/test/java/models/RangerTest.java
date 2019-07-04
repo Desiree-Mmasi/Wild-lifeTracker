53ac62ad25c57668d3a4926d7bd2ca1e939bf1f1
Moringa School <mmasidesiree9@gmail.com>
2019-07-04 12:23:36 +0300

    This commit will add the test for the feedback form

--- src/test/java/models/RangerTest.java
+++ src/test/java/models/RangerTest.java
@@ -17,13 +17,21 @@ public class RangerTest {
         Ranger ranger = new Ranger("John",7,7390213,"NorthWest");
         assertEquals(true, ranger instanceof Ranger);
     }
-    @After
-    public void tearDown () throws Exception {
-    }
+
     @Test
     public void newSightings_ObjectGetsCorrectlyCreated()
             throws Exception{
         Sightings sightings = new Sightings("NorthWest", "Our Pride Lies here!!");
         assertEquals(true, sightings instanceof Sightings);
     }
+    @Test
+    public void newFeedback_ObjectGetsCorrectlyCreated()
+        throws Exception{
+        Feedback feedback = new Feedback("Des", "Response", "Submit");
+        assertEquals(true, feedback instanceof Feedback);
+    }
+    @After
+    public void tearDown () throws Exception {
+    }
+
 }
