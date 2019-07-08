9f2d3587c4de69d6be9ca8f296ee4a7a1d790a18
Moringa School <mmasidesiree9@gmail.com>
2019-07-08 09:46:12 +0300

    This commit will add the timestamp to the sightings.java class

--- src/test/java/models/Sightings.java
+++ src/test/java/models/Sightings.java
@@ -3,17 +3,23 @@ package models;
 
 import org.sql2o.Connection;
 
+import java.sql.Timestamp;
+import java.text.DateFormat;
+import java.util.Date;
+
 public class Sightings {
   private int id;
   private int rangerId;
   private int animalId;
   private String location;
+  private Timestamp date;
 
-    public Sightings (int id, int rangerId, int animalId, String location) {
+    public Sightings (int id, int rangerId, int animalId, String location, Timestamp date) {
         this.id = id;
         this.rangerId = rangerId;
         this.animalId = animalId;
         this.location = location;
+        this.date = new Timestamp(new Date().getTime());
     }
 
     public int getId () {
@@ -31,6 +37,11 @@ public class Sightings {
     public String getLocation () {
         return location;
     }
+    public String getDate(){
+        return DateFormat.getDateInstance().format(this.date);
+    }
+
+
     public String getRangerName() {
         try(Connection con = DB.sql2o.open()) {
             return con.createQuery("SELECT name FROM rangers WHERE id=:id")
@@ -41,15 +52,25 @@ public class Sightings {
     public String getAnimalName() {
         try(Connection con = DB.sql2o.open()) {
             return con.createQuery("SELECT name FROM species WHERE id=:id")
-                    .addParameter("id", this.rangerId)
+                    .addParameter("id", this.animalId)
                     .executeAndFetchFirst(String.class);
         }
     }
-    public String getLocationName() {
-        try(Connection con = DB.sql2o.open()) {
-            return con.createQuery("SELECT name FROM sightings WHERE id=:id")
-                    .addParameter("id", this.rangerId)
-                    .executeAndFetchFirst(String.class);
+
+    public void save(){
+        try(Connection con = DB.sql2o.open()){
+            String sql = "INSERT INTO sightings (animalId, rangerId, location, date) VALUES (:animalId," +
+                    " " +
+                    ":rangerId, :location, :date)";
+            this.id = (int) con.createQuery(sql,true)
+                    .addParameter("animalId", this.animalId)
+                    .addParameter("rangerId", this.rangerId)
+                    .addParameter("location", this.location)
+                    .addParameter("date", this.date)
+                    .throwOnMappingFailure(false)
+                    .executeUpdate()
+                    .getKey();
         }
     }
+
 }
